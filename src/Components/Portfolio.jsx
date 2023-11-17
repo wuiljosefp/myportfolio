71ed95920be1fabcc1bdfa8bb6f572841600de58
wuiljosefp <138076162+wuiljosefp@users.noreply.github.com>
2023-11-17 04:16:31 +0000

    Updated portfolio image file extension from .jpeg to .jpg.

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/web-developer-working.jpeg";
+import image from "../images/web-developer-working.jpg";
 
 const imageAltText = "Web Developer working on a project";
 
